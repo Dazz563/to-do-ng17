3700dc4bba06614e0859b9959bc22d743b9446ea
Darren <58746026+Dazz563@users.noreply.github.com>
2024-01-22 10:47:08 +0200

    view hide password

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,26 +1,42 @@
+import {CommonModule} from '@angular/common';
 import {Component} from '@angular/core';
-import {FormGroup, FormControl, Validators, ReactiveFormsModule} from '@angular/forms';
+import {FormGroup, FormControl, Validators, ReactiveFormsModule, AbstractControl} from '@angular/forms';
 
 @Component({
 	selector: 'app-login',
 	standalone: true,
-	imports: [ReactiveFormsModule],
+	imports: [ReactiveFormsModule, CommonModule],
 	templateUrl: './login.component.html',
 })
 export class LoginComponent {
 	isLogin = true;
+	showPassword = false;
 
 	loginForm = new FormGroup({
 		email: new FormControl('', [Validators.required, Validators.email]),
 		password: new FormControl('', [Validators.required, Validators.minLength(6)]),
 	});
 
-	registerForm = new FormGroup({
-		firstName: new FormControl('', [Validators.required]),
-		lastName: new FormControl('', [Validators.required]),
-		email: new FormControl('', [Validators.required, Validators.email]),
-		password: new FormControl('', [Validators.required, Validators.minLength(6)]),
-	});
+	registerForm = new FormGroup(
+		{
+			firstName: new FormControl('', [Validators.required]),
+			lastName: new FormControl('', [Validators.required]),
+			email: new FormControl('', [Validators.required, Validators.email]),
+			password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+			confirmPassword: new FormControl('', [Validators.required]),
+		},
+		{
+			validators: this.passwordMatchValidator,
+		}
+	);
+
+	// Custom validator function
+	passwordMatchValidator(control: AbstractControl) {
+		const password = control.get('password')?.value;
+		const confirmPassword = control.get('confirmPassword')?.value;
+
+		return password === confirmPassword ? null : {mismatch: true};
+	}
 
 	submitLogin() {
 		console.log(this.loginForm.value);
@@ -32,7 +48,7 @@ export class LoginComponent {
 	}
 
 	submitRegister() {
-		console.log(this.loginForm.value);
+		console.log(this.registerForm.value);
 		if (this.registerForm.valid) {
 			console.log('Form Submitted!', this.registerForm.value);
 		} else {
