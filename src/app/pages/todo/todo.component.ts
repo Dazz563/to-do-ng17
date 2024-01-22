17083093bf2eeb69dc1836dc0e19036877af131a
Darren <58746026+Dazz563@users.noreply.github.com>
2024-01-22 15:55:24 +0200

    slide animation complete

--- src/app/pages/todo/todo.component.ts
+++ src/app/pages/todo/todo.component.ts
@@ -2,15 +2,23 @@ import {Component, Inject, OnInit} from '@angular/core';
 import {TodoCardComponent} from '../../shared/components/todo-card/todo-card.component';
 import {TodoService} from '../../services/todo.service';
 import {ITodo} from '../../models/todo.model';
+import {SlidePanelComponent} from '../../shared/ui/slide-panel/slide-panel.component';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
 	selector: 'app-todo',
 	standalone: true,
-	imports: [TodoCardComponent],
+	imports: [TodoCardComponent, SlidePanelComponent],
 	templateUrl: './todo.component.html',
 })
 export class TodoComponent implements OnInit {
+	todoForm = new FormGroup({
+		title: new FormControl('', [Validators.required]),
+		description: new FormControl('', [Validators.required]),
+		status: new FormControl('OPEN', [Validators.required]),
+	});
 	todos: ITodo[] = [];
+	isSlidePanelOpen = false;
 
 	constructor(private todoService: TodoService) {}
 
@@ -20,5 +28,13 @@ export class TodoComponent implements OnInit {
 
 	getAllTodos() {
 		this.todos = this.todoService.getAllTodos();
+	}
+
+	openSlidePanel() {
+		this.isSlidePanelOpen = true;
+	}
+
+	onCloseSlidePanel() {
+		this.isSlidePanelOpen = false;
 	}
 }
--- src/app/shared/ui/slide-panel/slide-panel.component.ts
+++ src/app/shared/ui/slide-panel/slide-panel.component.ts
@@ -1,9 +1,30 @@
-import {Component} from '@angular/core';
+import {animate, style, transition, trigger} from '@angular/animations';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 
 @Component({
 	selector: 'app-slide-panel',
 	standalone: true,
 	imports: [],
 	templateUrl: './slide-panel.component.html',
+	animations: [
+		trigger('fadeSlideInOut', [
+			transition(':enter', [
+				//
+				style({opacity: 0, transform: 'translateX(100%)'}),
+				animate('300ms', style({opacity: 1, transform: 'translateX(0%)'})),
+			]),
+			transition(':leave', [animate('300ms', style({opacity: 0, transform: 'translateX(100%)'}))]),
+		]),
+	],
 })
-export class SlidePanelComponent {}
+export class SlidePanelComponent {
+	@Input() isOpen = false;
+	@Input() headerText = 'Slide Panel Header';
+	@Output() onClose = new EventEmitter();
+
+	onClosePanel() {
+		// console.log('triggered');
+
+		this.onClose.emit(false);
+	}
+}
