a46acfffac34006f88aaacc9260a79a585b9f5fb
Darren <58746026+Dazz563@users.noreply.github.com>
2024-01-23 09:57:53 +0200

    updates to start using api

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,16 @@
+export const environment = {
+	production: false,
+	apiBaseUrl: 'http://localhost:3000',
+	apiEndpoints: {
+		login: '/api/login',
+		register: '/api/register',
+		logout: '/api/logout',
+		getUser: '/api/user',
+
+		getAllTodos: '/api/todos',
+		getTodoById: '/api/todos/:id',
+		createTodo: '/api/todos',
+		updateTodo: '/api/todos/:id',
+		deleteTodo: '/api/todos/:id',
+	},
+};
